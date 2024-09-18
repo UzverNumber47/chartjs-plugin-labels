c58c24d4bac30e30919a732dda3c3151ea2e2457
UzverNumber47 <dm.borisevich@gmail.com>
2024-09-18 15:30:13 +0200

    remove console.log

--- src/chartjs-plugin-labels.js
+++ src/chartjs-plugin-labels.js
@@ -242,7 +242,6 @@
   };
 
   Label.prototype.getFontSize = function (index) {
-    console.log('getFontSize function call');
     var fontSize = this.options.fontSize;
     if (typeof fontSize === 'function') {
       fontSize = fontSize(index);
